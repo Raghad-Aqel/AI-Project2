433b4aac3016dfd9fbe6021cb2e5c45d7f74a5ad
SajaAlkhalili <135491864+SajaAlkhalili@users.noreply.github.com>
2024-01-12 15:15:00 +0200

    version2

---
+++ src/main/java/com/example/demo2/Mark.java
@@ -0,0 +1,27 @@
+package com.example.demo2;
+
+public enum Mark {
+    X('X'),
+    O('O'),
+    BLANK(' ');
+
+    private final char mark;
+
+    Mark(char initMark) {
+        this.mark = initMark;
+    }
+
+    public boolean isMarked() {
+        return this != BLANK;
+    }
+
+    public char getMark() {
+        return this.mark;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(mark);
+    }
+}
+
